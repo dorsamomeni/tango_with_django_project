5890d0c7114ed13c1896d8eac96d360f5de63997
“Dorsa <2668095m@student.gla.ac.uk>
2023-02-08 12:14:12 +0000

    error chp.7

--- rango/forms.py
+++ rango/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-        name = forms.CharField(max_length=128,
-                                help_text="Please enter the category name.")
+        name = forms.CharField(max_length=128, help_text="Please enter the category name.")
         views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
         likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
         slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,11 +14,21 @@ class CategoryForm(forms.ModelForm):
                 fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://'
+        # then prepend 'http://'
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            
+        return cleaned_data
 
     class Meta:
         # Provide an association between the ModelForm and a model
@@ -32,4 +41,6 @@ class PageForm(forms.ModelForm):
         # we can either exclude the category field from the form,
         exclude = ('category',)
         # or specify the fields to include (don't include category field).
-        #fields = ('title', 'url', 'views')
+        #fields = ('title', 'url', 'views')   
+
+       
